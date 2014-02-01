f0bacfb7b7086a576aa2a26ff41df064d306b9bf
Crisu83 <ChristofferNiska@gmail.com>
2014-02-02 01:24:16 +0200

    Fix parsing of client response

--- src/NordSoftware/Paytrail/Http/Client.php
+++ src/NordSoftware/Paytrail/Http/Client.php
@@ -70,7 +70,7 @@ class Client extends Object
             throw new PaymentFailed('Server returned a non-JSON result.');
         }
 
-        $body = json_decode($response->getMessage());
+        $body = json_decode(substr($response->getMessage(), strpos($response->getMessage(), '{')));
 
         if ($response->getStatusCode() !== 201) {
             throw new PaymentFailed(
