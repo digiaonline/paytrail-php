023b217dd90e99ed3d97e800d9dd9b33cec42af0
Kenneth Söderlund <kenneth.soderlund@nordsoftware.com>
2016-03-23 21:11:15 +0200

    Rename namespace, add comments

--- src/Exception/CurrencyNotSupported.php
+++ src/Exception/CurrencyNotSupported.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of Paytrail.
  *
  * (c) 2013 Nord Software
@@ -8,10 +8,16 @@
  * file that was distributed with this source code.
  */
 
-namespace NordSoftware\Paytrail\Exception;
+namespace Paytrail\Exception;
 
-use NordSoftware\Paytrail\Common\Exception;
+use Paytrail\Common\Exception;
 
+/**
+ * Class CurrencyNotSupported.
+ *
+ * @package Paytrail\Exception
+ */
 class CurrencyNotSupported extends Exception
 {
+
 }
