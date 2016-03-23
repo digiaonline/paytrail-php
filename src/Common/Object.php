290073cf9946dbc787889056684ba991a6c11ecd
Kenneth Söderlund <kenneth.soderlund@nordsoftware.com>
2016-03-23 21:14:59 +0200

    Updates to Object
    - Re-name namespace
    - Add comments
    - Add logic to magic getter and setter

--- src/Common/Object.php
+++ src/Common/Object.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of Paytrail.
  *
  * (c) 2013 Nord Software
@@ -8,44 +8,72 @@
  * file that was distributed with this source code.
  */
 
-namespace NordSoftware\Paytrail\Common;
+namespace Paytrail\Common;
 
-use NordSoftware\Paytrail\Exception\PropertyDoesNotExist;
+use Paytrail\Exception\PropertyDoesNotExist;
 
+/**
+ * Class Object.
+ *
+ * @package Paytrail\Common
+ */
 abstract class Object
 {
+
     /**
-     * @param string $name
+     * Magic getter for the object property.
+     *
+     * @param string $name Name of the property to get.
+     *
      * @return mixed
+     *
+     * @throws PropertyDoesNotExist
      */
     public function __get($name)
     {
         $getter = 'get' . $name;
         if (method_exists($this, $getter)) {
             return $this->$getter();
+        } else if (property_exists($this, $name)) {
+            return $this->{$name};
         }
+        throw new PropertyDoesNotExist(sprintf('Property "%s" does not exist in %s.', $name, get_called_class()));
     }
 
     /**
-     * @param string $name
-     * @param mixed $value
+     * Magic setter for the object property.
+     *
+     * @param string $name  The property name to set.
+     * @param mixed  $value The value to set.
+     *
+     * @throws PropertyDoesNotExist
      */
     public function __set($name, $value)
     {
         $setter = 'set' . $name;
         if (method_exists($this, $setter)) {
             $this->$setter($value);
+        } else if (property_exists($this, $name)) {
+            $this->{$name} = $value;
         }
+        throw new PropertyDoesNotExist(sprintf('Property "%s" does not exist in %s.', $name, get_called_class()));
     }
 
     /**
-     * @param array $properties
-     * @throws \NordSoftware\Paytrail\Exception\PropertyDoesNotExist
+     * Configure the object.
+     *
+     * @param array $properties List of properties to set.
+     *
+     * @throws PropertyDoesNotExist
      */
     public function configure($properties = array())
     {
         foreach ($properties as $name => $value) {
-            $this->$name = $value;
+            if ( ! property_exists($this, $name)) {
+                throw new PropertyDoesNotExist(sprintf('Property "%s" does not exist in %s', $name,
+                    get_called_class()));
+            }
+            $this->{$name} = $value;
         }
     }
 }
